b02150f76c0965a7702b3e7ee368cbdd48cefb77
samuel_parkin <samuelparkin1@gmail.com>
2021-10-28 14:10:57 +1000

    updated __inti to include put and delete

--- vehicle_register/__init__.py
+++ vehicle_register/__init__.py
@@ -2,35 +2,51 @@ from flask import Flask
 
 app = Flask (__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home_page():
     return "A table of vehicles"
 
-@app.route('/staff')
+@app.route('/staff',methods=["GET"])
 def get_staff():
     return "A list of all registered staff members"
 
-@app.route('/staff/sign_up')
+@app.route('/staff/sign_up',  methods=["POST"])
 def post_staff_sign_up():
     return "Make a profile for a staff member"
 
-@app.route('/staff/<int:id>/')
+@app.route('/staff/<int:id>/', methods=["GET"])
 def get_staff_information(id):
-    return f" staff information for user {id}\n""list of staff members information"
+    return f" staff information for user {id}"
 
-@app.route('/vehicles')
+@app.route("/vehicles/<int:id>/", methods=["DELETE"])
+def staff_information_delete(id):
+    return f" staff information for {id} has been deleted"
+
+@app.route("/staff/<int:id>/", methods=["PUT", "PATCH"])
+def staff_information_update(id):
+    return f" staff information for user {id} has been updated"
+
+@app.route('/vehicles', methods=["GET"])
 def get_vehicles():
     return "A list of all registered vehicles"
 
-@app.route('/vehicles/new_vehicle')
+@app.route('/vehicles/new_vehicle', methods=["POST"])
 def post_new_vehicles():
     return "Make a profile for a new vehicle"
 
-@app.route('/vehicles/<int:id>/')
+@app.route('/vehicles/<int:id>/', methods=["GET"])
 def get_vehicle_information(id):
     return f" vehicle information for {id}\n""list of vehicle information"
 
-@app.route('/vehicles/<int:id>/photo')
+@app.route("/vehicles/<int:id>/", methods=["PUT", "PATCH"])
+def vehicles_information_update(id):
+    return f" vehicle information for {id} has been updated"
+
+@app.route("/vehicles/<int:id>/", methods=["DELETE"])
+def vehicles_information_delete(id):
+    return f" vehicle information for {id} has been deleted"
+
+@app.route('/vehicles/<int:id>/photo',  methods=["POST"])
 def post_vehicle_photos(id):
     return f" photos of {id}"
 
